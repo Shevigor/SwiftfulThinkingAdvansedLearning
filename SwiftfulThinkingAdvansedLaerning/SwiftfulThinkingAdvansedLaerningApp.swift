9f405105bf13203b88889e98f6f68fe4d35f6758
Igor Sheverda <ktgtoit@gmail.com>
2023-07-03 14:37:07 +0300

    Initial Commit

---
+++ SwiftfulThinkingAdvansedLaerning/SwiftfulThinkingAdvansedLaerningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvansedLaerningApp.swift
+//  SwiftfulThinkingAdvansedLaerning
+//
+//  Created by user on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvansedLaerningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
